2c397e3573a11eea5c1ae8b66a1ad63d710fbac0
Franco Cossatti <frcosati@hotmail.com>
2024-02-23 19:45:50 -0300

    Update route.ts

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 import bcrypt from 'bcrypt'
 import db from "@/app/libs/db"
 
-async function POST(request: { json: () => any }) {
+export async function POST(request: { json: () => any }) {
     try {
         const data = await request.json()
     
